b1db8452bd3298cc500eece09aec39dc5b775da7
annagudrun <annag14@ru.is>
2016-05-08 17:05:27 +0000

    þættirnir færast yfir í /Shows/nafnáþætti :)

--- sortDirectory.py
+++ sortDirectory.py
@@ -7,7 +7,7 @@ def sortDirectory(dir):
 
     path = input('Full path to folder: ')
     search = input('Name of show: ')
-    folderShowName = input('Name of folder: ')
+    episode_folder = input('Name of folder: ')
    #gera edge case her ef tetta skyldi ekki vera rett
    #if not os.path.isdir(path):
 
@@ -22,31 +22,30 @@ def sortDirectory(dir):
     else:
        showFolder = os.path.join(path,'Shows')
     print(showFolder)
+    
     #labba svo i gegnum allt og tekka hvort tad se med tatta endingu
-
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(VIDEO_TYPES):
-                file_path = os.path.join(root, file)
-                hit = find_file(search, file) #gefur okkur true eda false eftir tvi hvort eitthvad finnist
-                if hit:
-                    beforeMove(file, file_path, folderShowName, showFolder)
+                file_path = os.path.join(root, file)                                              
+                found = find_file(search, file)
+                # found gefur true ef tatturinn passar vid leitar streng
+                #ta buum vid til moppu og faerum tattinn yfir
+                #notandi velur nafn a moppunni 
+                if found:
+                    newPath = showFolder + '/' + episode_folder
+                    if not os.path.exists(newPath):
+                        os.mkdir(newPath)
+                    if not os.path.exists(newPath +  '/' + file):
+                        os.rename(file_path, newPath +  '/' + file)
+                        print(file + " MOVED TO " + newPath)
 
 
-def find_file(query, file_name):
+def find_file(input_query, file_name):
     file_name = file_name.replace('_', '')
-    query = re.sub(r'\W+', '', query) #taka ut non alpha symbolds
+    input_query = re.sub(r'\W+', '', input_query) #taka ut non alpha symbolds
     file_name = re.sub(r'\W+', '', file_name)
-    if query.lower() in file_name.lower():
+    if input_query.lower() in file_name.lower():
         return True
 
     return False
-
-def beforeMove(file, file_path, folderShowName, showFolder):
-    seasons = re.search('[Ss]\s*[0-9]+\s*[Ee]\s*[0-9]+',file)
-    seasons1 = re.search('\[[0-9]+x[0-9]+\]|[0-9]+x[0-9]+',file)
-
-    new_file_path = showFolder + '/' + folderShowName
-    print('newfilepath ' + new_file_path)
- #   os.mkdir(new_file_path)
- 
