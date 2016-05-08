717774d7ea49cacbb25bafac5e611f522de13a23
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2016-05-08 22:36:51 +0000

    allt blandad saman

--- sortDirectory.py
+++ sortDirectory.py
@@ -1,19 +1,20 @@
 import os
 import re
+import shutil
 
 VIDEO_TYPES = ('.wmv', '.mov', '.avi', '.divx', '.mpeg', '.mpg', '.m4p', '.3gp', '.amv', '.qt', '.rm', '.swf', '.mp4', '.mkv', '.mp3', '.srt', '.sup')
-TRASH = ('nfo', 'txt', 'dat', 'jpg','torrent','ini' )
+TRASH = ('.nfo', '.txt', '.dat', '.jpg','.torrent','.ini', '.mta' )
 
 def inputPath():
-    path = input('Full path to folder: ')
+    path = input('Enter full path to folder: ')
     while not os.path.exists(path):
         print('Invalid path, try again')
-        path = input('Path to folder: ')
+        path = input('Enter full path to folder: ')
     return path
 
 def sortDirectory(path):
 
- #   path = input('Full path to folder: ')
+    path = input('Full path to folder: ')
     search = input('Name of show: ')
     episode_folder = input('Name of folder: ')
    #gera edge case her ef tetta skyldi ekki vera rett
@@ -45,9 +46,13 @@ def sortDirectory(path):
                     if not os.path.exists(newPath):
                         os.mkdir(newPath)
                     if not os.path.exists(newPath +  '/' + file):
+                        print('newpath!!!: ' + newPath)
+ #                       shutil.move(file, newPath)
                         os.rename(file_path, newPath +  '/' + file)
                         print(file + " MOVED TO " + newPath)
-    delete(path)
+                        
+    move_to_season_folders(newPath)
+    delete(path)   
 
 def find_file(input_query, file_name):
     file_name = file_name.replace('_', '')
@@ -55,8 +60,32 @@ def find_file(input_query, file_name):
     file_name = re.sub(r'\W+', '', file_name)
     if input_query.lower() in file_name.lower():
         return True
-
     return False
+
+def move_to_season_folders(episode_folder):
+
+    os.chdir(episode_folder)
+    print('episode folder: ' + episode_folder)
+    for root,dirs,files in os.walk(episode_folder):
+        for file in files:
+            seas_type1 = re.search(r'[sS]\d+',file)
+            seas_type2 = re.search(r'\[[0-9]+x\d+|[0-9]+x\d+',file)
+            file_path = os.path.join(episode_folder, file)
+            if seas_type1:
+                seas = seas_type1.group()
+                if not os.path.exists(os.path.join(episode_folder,seas)):
+                    new_folder = os.mkdir(os.path.join(episode_folder,seas))
+                else:
+                    new_folder = os.path.join(episode_folder,seas)
+                    shutil.move(file_path, new_folder)
+                    print('newfold: ' + new_folder)
+            elif seas_type2:
+                seas = seas_type2.group()
+                if not os.path.exists(os.path.join(episode_folder,seas)):
+                    new_folder = os.mkdir(os.path.join(episode_folder,seas))
+                else:
+                    new_folder = os.path.join(episode_folder,seas)
+                    shutil.move(file_path, new_folder)
 
 def delete(path):
     #remove files with trash endings like .nfo,.torrent, etc.
@@ -72,16 +101,13 @@ def removeEmpty(path):
         return
     if os.path.isdir(path):
             f = os.listdir(path)
-            if len(f):
+            if len(f)>0:
                 for file in f:
                     f_path = os.path.join(path,file)
                     if os.path.isdir(f_path):
                         removeEmpty(f_path)
-
             if len(f) == 0:
-                print("Remove empty :", path)
+                print("Removing: ", path)
                 os.rmdir(path)
-
-url = inputPath()
-sortDirectory(url)
-
+                
+sortDirectory(inputPath())
